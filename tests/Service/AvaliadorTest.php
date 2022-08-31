310f9e01f74637db21ad72e160269176eefc280d
João Passareli <joaopassareli@gmail.com>
2022-08-31 19:53:40 -0300

    Criando o primeiro teste unitário, onde verifica se o valor do maior lance está sendo atribuído corretamente

---
+++ tests/Service/AvaliadorTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Alura\Leilao\tests\Service;
+
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use PHPUnit\Framework\TestCase;
+use Alura\Leilao\Service\Avaliador;
+
+class AvaliadorTest extends TestCase
+{
+    public function testeAvaliadorDeveEncontrarMaiorValorDeLances ()
+    {
+        // 
+        $leilao = new Leilao('Ford Ka 2020 0KM');
+
+        $joao = new Usuario('Joao');
+        $bruna = new Usuario('Bruna');
+
+        $leilao->recebeLance(new Lance($joao, 2000));
+        $leilao->recebeLance(new Lance($bruna, 3000));
+
+        $leiloeiro = new Avaliador();
+
+        $leiloeiro->avalia($leilao);
+        $maiorValor = $leiloeiro->getMaiorValor();
+
+        // Assert
+        self::assertEquals(3000, $maiorValor);
+    }
+}
