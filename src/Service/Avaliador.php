118e172e56695a9326c5eb969b46711574106ca7
Joao Passareli <joaopassareli@gmail.com>
2022-09-01 10:04:57 -0300

    Implementando novos testes e melhorando o codigo da classe Avaliador.

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -7,6 +7,8 @@ use Alura\Leilao\Model\Leilao;
 class Avaliador
 {
     private $maiorValor = 0;
+    private $menorValor = INF;
+    private $maioresLances;
 
     public function avalia (Leilao $leilao):void
     {        
@@ -15,12 +17,38 @@ class Avaliador
             {
                 $this->maiorValor = $lance->getValor();
             }
+
+            if($lance->getValor() < $this->menorValor) {
+                $this->menorValor = $lance->getValor();
+            }
         }
+
+        $leilao - $leilao->getLances();
+        \usort($lances, function(Lance $lance1, Lance $lance2){
+            return $lance1->getValor() - $lance2->getValor();
+        });
+
+        // com o metodo array_slice, é selecionado o array (lances) e dele, será iniciado do primeiro valor(0) e retornado 
+        // os três primeiros valores (3). Ou seja, será retornado os três maiores lances em ordem decrescente.
+        $this->maioresLances = array_slice($lances, 0, 3);
     }
 
     public function getMaiorValor(): float
     {
         return $this->maiorValor;
+    }
+
+    public function getMenorValor(): float
+    {
+        return $this->menorValor;
+    }
+
+    /**
+     * @return Lance[]
+     */
+    public function getMaioresLances(): array
+    {
+        return $this->maioresLances;
     }
 }
 
--- tests/Service/AvaliadorTest.php
+++ tests/Service/AvaliadorTest.php
@@ -8,6 +8,7 @@ use Alura\Leilao\Model\Usuario;
 use PHPUnit\Framework\TestCase;
 use Alura\Leilao\Service\Avaliador;
 
+
 class AvaliadorTest extends TestCase
 {
     public function testeAvaliadorDeveEncontrarMaiorValorDeLances ()
@@ -28,5 +29,26 @@ class AvaliadorTest extends TestCase
 
         // Assert
         self::assertEquals(3000, $maiorValor);
+    }
+
+    public function testeAvaliadorDeveEncontrarMenorValorDeLances ()
+    {
+        // Arrange - Given
+        $leilao = new Leilao('Ford Ka 2020 0KM');
+
+        $joao = new Usuario('Joao');
+        $bruna = new Usuario('Bruna');
+
+        $leilao->recebeLance(new Lance($joao, 2000));
+        $leilao->recebeLance(new Lance($bruna, 3000));
+
+        $leiloeiro = new Avaliador();
+
+        // Act - When
+        $leiloeiro->avalia($leilao);
+        $menorValor = $leiloeiro->getMenorValor();
+
+        // Assert - Then
+        self::assertEquals(2000, $menorValor);
     }
 }
